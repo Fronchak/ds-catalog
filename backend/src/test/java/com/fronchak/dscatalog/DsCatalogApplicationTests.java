60b80f49711eaef1912f5a05dfc6448134be23ec
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-11-28 00:04:53 -0300

    Create project

---
+++ backend/src/test/java/com/fronchak/dscatalog/DsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fronchak.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
