4d1eb37a4d94074fa2b10ee6413905b70af1262a
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-11-30 20:51:58 -0300

    Implement assosiation n x n between product and category

--- backend/src/main/java/com/fronchak/dscatalog/domain/entities/Category.java
+++ backend/src/main/java/com/fronchak/dscatalog/domain/entities/Category.java
@@ -14,7 +14,7 @@ import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "category")
+@Table(name = "tb_category")
 public class Category implements Serializable {
 
 	private static final long serialVersionUID = 1L;
