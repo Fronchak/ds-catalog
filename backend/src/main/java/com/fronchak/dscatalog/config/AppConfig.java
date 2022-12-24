4db7ee13f924780bdc60be435ef63be9f4d72e05
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-12-23 21:11:24 -0300

    Implemented token additional information

--- backend/src/main/java/com/fronchak/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/fronchak/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.fronchak.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,9 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
 	@Bean
 	BCryptPasswordEncoder passwordEnconder() {
 		return new BCryptPasswordEncoder();
@@ -17,7 +21,7 @@ public class AppConfig {
 	@Bean
 	JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
